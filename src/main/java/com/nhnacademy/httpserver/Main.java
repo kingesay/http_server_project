a73306888170089e48ab764cf5026e6617342d97
wskim <wskim0605@naver.com>
2022-04-21 17:06:01 +0900

    메인클래스 작성

---
+++ src/main/java/com/nhnacademy/httpserver/Main.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.httpserver;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        try (var context =
+                 new AnnotationConfigApplicationContext("com.nhnacademy.httpserver")) {
+//
+        }
+    }
+}
