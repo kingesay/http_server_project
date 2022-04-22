ee014f03d82af9f745d60b59c1c2626a230c6596
wskim <wskim0605@naver.com>
2022-04-22 14:50:10 +0900

    body test 작성

--- src/main/java/com/nhnacademy/httpserver/parser/args/Args.java
+++ src/main/java/com/nhnacademy/httpserver/parser/args/Args.java
@@ -1,5 +1,15 @@
 package com.nhnacademy.httpserver.parser.args;
 
-public class Args {
+import java.util.Map;
 
+public class Args {
+    private final Map<String, String> argsBodyData;
+
+    public Args(Map<String, String> jsonBodyData) {
+        this.argsBodyData = jsonBodyData;
+    }
+
+    public Map<String, String> getJsonBodyData() {
+        return argsBodyData;
+    }
 }
--- src/test/java/com/nhnacademy/httpserver/body/BodyTest.java
+++ src/test/java/com/nhnacademy/httpserver/body/BodyTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.nhnacademy.httpserver.parser.json.JsonBody;
 import com.nhnacademy.httpserver.parser.json.BodyParser;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +27,24 @@ public class BodyTest {
         assertThat(body.getJsonBodyData().toString()).isEqualTo(
             "{msg1=jack, msg2=jack}"
         );
+    }
+
+    @Test
+    void get_method_argument_test() {
+        String bodyData = "/get?msg1=hello&msg2=world";
+
+        StringTokenizer tokenizer = new StringTokenizer(bodyData ,"?");
+        tokenizer.nextToken();
+        StringTokenizer tokenizer2 = new StringTokenizer(tokenizer.nextToken() ,"&");
+        Map<String, String> msgMap = new HashMap<>();
+        while(tokenizer2.hasMoreTokens()) {
+            String msg = tokenizer2.nextToken();
+            String[] arr = msg.split("=");
+            msgMap.put(arr[0], arr[1]);
+        }
+
+        assertThat(msgMap).containsEntry("msg1","hello");
+        assertThat(msgMap).containsEntry("msg2","world");
+
     }
 }
